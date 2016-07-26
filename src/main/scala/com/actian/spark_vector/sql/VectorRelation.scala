1f36fc72939546e6adbcb416b8f82cf8494d2d6a
Andrei Costea <and-costea@users.noreply.github.com>
2016-07-26 11:31:30 +0200

    Merge pull request #70 from ActianCorp/eliminate_useless_conv
    Eliminate useless RDD map when the input columns match the target columns during Spark-Vector load.
    
    Also replaced sc.parallelize with sc.range when generating dataframes with 0 columns (for count(*) type of queries).

--- src/main/scala/com/actian/spark_vector/sql/VectorRelation.scala
+++ src/main/scala/com/actian/spark_vector/sql/VectorRelation.scala
@@ -69,7 +69,7 @@ private[spark_vector] class VectorRelation(tableRef: TableRef,
           }
           val numRows = rs.getLong(1)
           logDebug(s"Create empty RDD from the count(*) of ${numRows} row(s).")
-          sqlContext.sparkContext.parallelize(1L to numRows).map(_ => InternalRow.empty).asInstanceOf[RDD[Row]]
+          sqlContext.sparkContext.range(0L, numRows).map(_ => InternalRow.empty).asInstanceOf[RDD[Row]]
         }
       }
     } else {
